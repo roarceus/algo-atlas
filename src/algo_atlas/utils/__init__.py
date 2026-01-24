34e38737bfb2aa726b6107b00f8309f0d443a4c4
Sohan Patil <sohan.patil98@gmail.com>
2026-01-23 19:46:44 -0500

    feat(utils): add file manager for vault operations

--- src/algo_atlas/utils/__init__.py
+++ src/algo_atlas/utils/__init__.py
@@ -1,5 +1,24 @@
 """Utility modules for AlgoAtlas."""
 
+from algo_atlas.utils.file_manager import (
+    check_problem_exists,
+    create_difficulty_folders,
+    create_problem_folder,
+    get_vault_path,
+    save_markdown,
+    save_solution_file,
+    validate_vault_repo,
+)
 from algo_atlas.utils.logger import Logger, get_logger
 
-__all__ = ["Logger", "get_logger"]
+__all__ = [
+    "Logger",
+    "get_logger",
+    "get_vault_path",
+    "validate_vault_repo",
+    "create_difficulty_folders",
+    "create_problem_folder",
+    "save_solution_file",
+    "save_markdown",
+    "check_problem_exists",
+]
